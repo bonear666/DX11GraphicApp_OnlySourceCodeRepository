5e9ad4f2a760dc62c368353110c74d4d27272713
cyberdaddy666 <98484798+cyberdaddy666@users.noreply.github.com>
2022-05-29 11:06:38 +0500

    Git Version 2

--- Shaders/TriangleVertexShader.hlsl
+++ Shaders/TriangleVertexShader.hlsl
@@ -1,4 +1,29 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+cbuffer MatrixesConstantBuffer : register(b0)
 {
-	return pos;
+    matrix World;
+    matrix View;
+    matrix Projection;
+};
+
+cbuffer angleBuf : register(b1)
+{
+    float angle;
+    float angle1;
+    float angle2;
+    float angle3;
+};
+
+float4 main(float4 pos : POSITION) : SV_POSITION
+{
+    float4 newPos;
+
+newPos.x = pos.x * cos(angle) - pos.y * sin(angle);
+newPos.y = pos.x * sin(angle) + pos.y * cos(angle);
+newPos.z = pos.z;
+newPos.w = pos.w;
+
+newPos = mul(newPos, World); 
+newPos = mul(newPos, View); 
+newPos = mul(newPos, Projection);
+	return newPos;
 }
