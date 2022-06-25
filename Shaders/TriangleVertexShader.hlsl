47f5d9facf4bcd44a05171d21c95fba38dce261e
cyberdaddy666 <98484798+cyberdaddy666@users.noreply.github.com>
2022-06-25 23:23:33 +0500

    VS Version 2

--- Shaders/TriangleVertexShader.hlsl
+++ Shaders/TriangleVertexShader.hlsl
@@ -1,8 +1,8 @@
 cbuffer MatrixesConstantBuffer : register(b0)
 {
-    matrix world;
-    matrix view;
-    matrix projection;
+    float4x4 world;
+    float4x4 view;
+    float4x4 projection;
 };
 
 cbuffer angleConstantBuffer : register(b1)
@@ -21,12 +21,14 @@ struct VS_OUTPUT {
 VS_OUTPUT main(float4 pos : POSITION, float4 color : COLOR) 
 {
     float4 newPos;
-    // поворот вокруг точки 
+    
+    // поворот вокруг точки (0; 0; 0.5)
     newPos.x = pos.x * cos(angle) - (pos.z - 0.5) * sin(angle);
     newPos.y = pos.y;
     newPos.z = 0.5 + pos.x * sin(angle) + (pos.z - 0.5) * cos(angle);
     newPos.w = pos.w;
 
+    newPos = mul(view, newPos);
     newPos = mul(newPos, projection);
 
     VS_OUTPUT output;
